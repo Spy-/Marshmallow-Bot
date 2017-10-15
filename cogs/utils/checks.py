9219b9a8fcfafc3bc5365d293166eb77af434ae8
John <johnfish218@gmail.com>
2017-10-15 13:59:22 -0500

    fresh utils

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -1,148 +1,73 @@
-import asyncio
-
 from discord.ext import commands
-import discord
-from . import utilities
 
-loop = asyncio.get_event_loop()
+# The permission system of the bot is based on a "just works" basis
+# You have permissions and the bot has permissions. If you meet the permissions
+# required to execute the command (and the bot does as well) then it goes through
+# and you can execute the command.
+# Certain permissions signify if the person is a moderator (Manage Server) or an
+# admin (Administrator). Having these signify certain bypasses.
+# Of course, the owner will always be able to execute commands.
 
-# The tables needed for the database, as well as their primary keys
-required_tables = {
-    'battle_records': 'member_id',
-    'boops': 'member_id',
-    'command_usage': 'command',
-    'overwatch': 'member_id',
-    'picarto': 'member_id',
-    'server_settings': 'server_id',
-    'raffles': 'server_id',
-    'strawpolls': 'server_id',
-    'osu': 'member_id',
-    'tags': 'server_id',
-    'tictactoe': 'member_id',
-    'twitch': 'member_id',
-    'user_playlists': 'member_id',
-    'birthdays': 'member_id'
-}
-
-def is_owner(ctx):
-    if not hasattr(ctx.bot, "owner"):
-        return False
-    return ctx.bot.owner.id == ctx.message.author.id
-
-
-def should_ignore(bot, message):
-    if message.guild is None:
-        return False
-    ignored = bot.db.load('server_settings', key=message.guild.id, pluck='ignored')
-    if not ignored:
-        return False
-    return str(message.author.id) in ignored['members'] or str(message.channel.id) in ignored['channels']
-
-
-def check_restricted():
-    async def predicate(ctx):
-        # Return true if this is a private channel, we'll handle that in the registering of the command
-        if type(ctx.message.channel) is discord.DMChannel:
-            return True
-
-        # First get all the restrictions
-        restrictions = ctx.bot.db.load('server_settings', key=ctx.message.guild.id, pluck='restrictions') or {}
-        # Now lets check the "from" restrictions
-        for from_restriction in restrictions.get('from', []):
-            # Get the source and destination
-            # Source should ALWAYS be a command in this case
-            source = from_restriction.get('source')
-            destination = from_restriction.get('destination')
-            # Convert destination to the object we want
-            destination = await utilities.convert(ctx, destination)
-            # If we couldn't find the destination, just continue with other restrictions
-            # Also if this restriction we're checking isn't for this command
-            if destination is None or source != ctx.command.qualified_name:
-                continue
-
-            # This means that the type of restriction we have is `command from channel`
-            # Which means we do not want commands to be ran in this channel
-            if destination == ctx.message.channel:
-                return False
-            # This type is `command from Role` meaning anyone with this role can't run this command
-            elif destination in ctx.message.author.roles:
-                return False
-            # This is `command from Member` meaning this user specifically cannot run this command
-            elif destination == ctx.message.author:
-                return False
-
-        # If we are here, then there are no blacklists stopping this from running
-
-        # Now for the to restrictions this is a little different, we need to make a whitelist and
-        # see if our current channel is in this whitelist, as well as any whitelisted roles are in the author's roles
-        # Only if there is no whitelist, do we want to blanket return True
-        to_restrictions = restrictions.get('to', [])
-        if len(to_restrictions) == 0:
-            return True
-
-        # Otherwise there is a whitelist, and we need to start it
-        whitelisted_channels = []
-        whitelisted_roles = []
-
-        for to_restriction in to_restrictions:
-            # Get the source and destination
-            # Source should ALWAYS be a command in this case
-            source = to_restriction.get('source')
-            destination = to_restriction.get('destination')
-            # Convert destination to the object we want
-            destination = await utilities.convert(ctx, destination)
-            # If we couldn't find the destination, just continue with other restrictions
-            # Also if this restriction we're checking isn't for this command
-            if destination is None or source != ctx.command.qualified_name:
-                continue
-
-            # Append to our two whitelists depending on what type this is
-            if isinstance(destination, discord.TextChannel):
-                whitelisted_channels.append(destination)
-            elif isinstance(destination, discord.Role):
-                whitelisted_roles.append(destination)
-
-        if whitelisted_channels:
-            if ctx.channel not in whitelisted_channels:
-                return False
-        if whitelisted_roles:
-            if not any(x in ctx.message.author.roles for x in whitelisted_roles):
-                return False
-
-        # If we have passed all of these, then we are allowed to run this command
-        # This looks like a whole lot, but all of these lists will be very tiny in almost all cases
-        # And only delving deep into the specific lists that may be large, will we finally see "large" lists
-        # Which means this still will not be slow in other cases
+async def check_permissions(ctx, perms, *, check=all):
+    is_owner = await ctx.bot.is_owner(ctx.author)
+    if is_owner:
         return True
 
+    resolved = ctx.channel.permissions_for(ctx.author)
+    return check(getattr(resolved, name, None) == value for name, value in perms.items())
+
+def has_permissions(*, check=all, **perms):
+    async def pred(ctx):
+        return await check_permissions(ctx, perms, check=check)
+    return commands.check(pred)
+
+async def check_guild_permissions(ctx, perms, *, check=all):
+    is_owner = await ctx.bot.is_owner(ctx.author)
+    if is_owner:
+        return True
+
+    if ctx.guild is None:
+        return False
+
+    resolved = ctx.author.guild_permissions
+    return check(getattr(resolved, name, None) == value for name, value in perms.items())
+
+def has_guild_permissions(*, check=all, **perms):
+    async def pred(ctx):
+        return await check_guild_permissions(ctx, perms, check=check)
+    return commands.check(pred)
+
+# These do not take channel overrides into account
+
+def is_mod():
+    async def pred(ctx):
+        return await check_guild_permissions(ctx, {'manage_guild': True})
+    return commands.check(pred)
+
+def is_admin():
+    async def pred(ctx):
+        return await check_guild_permissions(ctx, {'administrator': True})
+    return commands.check(pred)
+
+def mod_or_permissions(**perms):
+    perms['manage_guild'] = True
+    async def predicate(ctx):
+        return await check_guild_permissions(ctx, perms, check=any)
     return commands.check(predicate)
 
+def admin_or_permissions(**perms):
+    perms['administrator'] = True
+    async def predicate(ctx):
+        return await check_guild_permissions(ctx, perms, check=any)
+    return commands.check(predicate)
 
-def custom_perms(**perms):
+def is_in_guilds(*guild_ids):
     def predicate(ctx):
-        # Return true if this is a private channel, we'll handle that in the registering of the command
-        if type(ctx.message.channel) is discord.DMChannel:
-            return True
-
-        # Get the member permissions so that we can compare
-        guild_perms = ctx.message.author.guild_permissions
-        channel_perms = ctx.message.author.permissions_in(ctx.message.channel)
-        # Currently the library doesn't handle administrator overrides..so lets do this manually
-        if guild_perms.administrator:
-            return True
-        # Next, set the default permissions if one is not used, based on what was passed
-        # This will be overriden later, if we have custom permissions
-        required_perm = discord.Permissions.none()
-        for perm, setting in perms.items():
-            setattr(required_perm, perm, setting)
-
-        required_perm_value = ctx.bot.db.load('server_settings', key=ctx.message.guild.id, pluck='permissions') or {}
-        required_perm_value = required_perm_value.get(ctx.command.qualified_name)
-        if required_perm_value:
-            required_perm = discord.Permissions(required_perm_value)
-
-        # Now just check if the person running the command has these permissions
-        return guild_perms >= required_perm or channel_perms >= required_perm
-
-    predicate.perms = perms
+        guild = ctx.guild
+        if guild is None:
+            return False
+        return guild.id in guild_ids
     return commands.check(predicate)
+
+def is_lounge_cpp():
+    return is_in_guilds(145079846832308224)
--- cogs/voice_utilities/__init__.py
+++
@@ -1,5 +0,0 @@
-from .downloader import Downloader
-from .exceptions import *
-from .playlist import Playlist
-from .source import *
-import opus_loader
