bc390ce204a0a706490354f738033b5f8269095a
John <johnfish218@gmail.com>
2017-10-18 22:00:31 -0500

    removed need for parameter in opus loader

--- cogs/voice_utilities/opus_loader.py
+++ cogs/voice_utilities/opus_loader.py
@@ -1,17 +1,20 @@
 from discord import opus
 
-OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll', 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
 
+def load_opus_lib():
 
-def load_opus_lib(opus_libs=OPUS_LIBS):
+    OPUS_LIBS = ['libopus-0.x86.dll', 'libopus-0.x64.dll',
+                 'libopus-0.dll', 'libopus.so.0', 'libopus.0.dylib']
+
     if opus.is_loaded():
         return True
 
-    for opus_lib in opus_libs:
+    for opus_lib in OPUS_LIBS:
         try:
             opus.load_opus(opus_lib)
             return
         except OSError:
             pass
 
-    raise RuntimeError('Could not load an opus lib. Tried %s' % (', '.join(opus_libs)))
+    raise RuntimeError('Could not load an opus lib. Tried %s' %
+                       (', '.join(OPUS_LIBS)))
