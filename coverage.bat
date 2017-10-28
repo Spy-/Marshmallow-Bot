0dc217fb82a7ea9a1ed16ad910c2380f980c7d84
John <johnfish218@gmail.com>
2017-10-28 16:40:40 -0500

    updated coverage bat

--- coverage.bat
+++ coverage.bat
@@ -1,3 +1,3 @@
-coverage xml
-set CODACY_PROJECT_TOKEN=8032b61c2d974b30b46bd1f7f19f95a5 
+coverage xml &&
+set CODACY_PROJECT_TOKEN=8032b61c2d974b30b46bd1f7f19f95a5 &&
 python-codacy-coverage -r coverage.xml
