b6c013fa5e571a616b119d856e1e531b82489102
John <johnfish218@gmail.com>
2017-11-01 23:31:32 -0500

    rewrote the queue functions as a static listify function

--- marshmallow/core/mechanics/music.py
+++ marshmallow/core/mechanics/music.py
@@ -1,10 +1,12 @@
-import os
-import discord
 import asyncio
-import hashlib
 import functools
-import youtube_dl
+import hashlib
+import os
+from asyncio.queues import Queue
 from concurrent.futures import ThreadPoolExecutor
+
+import discord
+import youtube_dl
 
 ytdl_params = {
     'format': 'bestaudio/best',
@@ -66,7 +68,10 @@ class QueueItem(object):
     async def create_player(self, voice_client):
         await self.download()
         audio_source = discord.FFmpegPCMAudio(self.location)
-        voice_client.play(audio_source)
+        audio_source = discord.PMCVolumeTransformer(audio_source)
+        audio_source = 0.6
+        if not voice_client.is_playing():
+            voice_client.play(audio_source)
 
 
 class MusicCore(object):
@@ -75,7 +80,6 @@ class MusicCore(object):
         self.db = bot.db
         self.loop = asyncio.get_event_loop()
         self.threads = ThreadPoolExecutor(2)
-        self.players = {}
         self.queues = {}
         self.currents = {}
         self.repeaters = []
@@ -91,24 +95,16 @@ class MusicCore(object):
         if guild_id in self.queues:
             queue = self.queues[guild_id]
         else:
-            queue = []
+            queue = Queue()
             self.queues.update({guild_id: queue})
         return queue
 
-    def queue_add(self, guild_id, requester, item_info):
-        if item_info is not None:
-            queue = self.get_queue(guild_id)
-            item = QueueItem(requester, item_info)
-            queue.append(item)
-            self.queues.update({guild_id: queue})
-
-    def queue_get(self, guild_id):
-        queue = self.get_queue(guild_id)
-        item = queue.pop(0)
-        self.queues.update({guild_id: queue})
-        return item
-
-    def queue_del(self, guild_id, order_number):
-        queue = self.get_queue(guild_id)
-        queue.remove(queue[order_number])
-        self.queues.update({guild_id: queue})
+    @staticmethod
+    async def listify_queue(queue):
+        item_list = []
+        while not queue.empty():
+            item = await queue.get()
+            item_list.append(item)
+        for item in item_list:
+            await queue.put(item)
+        return item_list
