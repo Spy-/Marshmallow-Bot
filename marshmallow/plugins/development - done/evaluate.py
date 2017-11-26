2102bd6c601597d86443c721f768345dce113bfc
Spy- <johnfish218@gmail.com>
2017-11-25 20:26:38 -0600

    evaluate command is actually useful now

--- marshmallow/plugins/development - done/evaluate.py
+++ marshmallow/plugins/development - done/evaluate.py
@@ -1,21 +1,49 @@
 import discord
 import inspect
-
+import io
+import textwrap
+import traceback
+from contextlib import redirect_stdout
 from marshmallow.core.utilities.constants import *
 
 
 async def evaluate(cmd, message, args):
-    if not args:
-        status = discord.Embed(color=ERROR, title='❗ Nothing Inputted To Process')
+    env = {
+        'bot': cmd.bot,
+        'channel': message.channel,
+        'author': message.author,
+        'guild': message.guild,
+        'message': message,
+        'args': args,
+        'cmd': cmd
+    }
+
+    env.update(globals())
+
+    stdout = io.StringIO()
+
+    body = " ".join(args)
+
+    to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+    try:
+        exec(to_compile, env)
+    except Exception as e:
+        return await message.channel.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+    func = env['func']
+    try:
+        with redirect_stdout(stdout):
+            ret = await func()
+    except Exception as e:
+        value = stdout.getvalue()
+        await message.channel.send(f'```py\n{value}{traceback.format_exc()}\n```')
     else:
+        value = stdout.getvalue()
         try:
-            execution = " ".join(args)
-            output = eval(execution)
-            if inspect.isawaitable(output):
-                output = await output
-            status = discord.Embed(title='✅ Executed', color=0x77B255)
-            status.add_field(name='Results', value=f'\n```\n{output}\n```')
-        except Exception as e:
-            status = discord.Embed(color=ERROR, title='❗ Error')
-            status.add_field(name='Execution Failed', value=f'{e}')
-    await message.channel.send(None, embed=status)
+            await message.channel.message.add_reaction('\u2705')
+        except:
+            pass
+        if ret is None:
+            if value:
+                await message.channel.send(f'```py\n{value}\n```')
